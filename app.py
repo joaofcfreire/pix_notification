f4ca838025f9ecad3b6ee12c8e038b1f053feaa8
joaofcfreire <joaofcfreire@hotmail.com>
2025-05-08 09:46:01 -0300

    Create database models

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
+from repository.database import db
+from models.payment import Payment
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db.init_app(app)
 
 @app.route("/payments/pix", methods=["POST"])
 def create_payment_pix():
@@ -11,7 +16,7 @@ def create_payment_pix():
 def pix_confirmation():
     return jsonify({"message": "The payment has been confirmed"})
 
-@app.route("/payments/pix/<int:payment_id", methods=["GET"])
+@app.route("/payments/pix/<int:payment_id>", methods=["GET"])
 def payment_pix_page(payment_id):
     return "Pagamento Pix"
 
